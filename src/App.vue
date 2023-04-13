2c4b5de7aa9205dca6ca379e4a48108300d71ffa
Thuc Tran <thuc.tv@techchain.vn>
2023-04-13 22:07:15 +0700

    feat: update ansible

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it! 0.0.11" />
+      <HelloWorld msg="You did it! 0.0.11 updated" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
