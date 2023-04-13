8105334f63b19a7f96a109318e4e150ea7ec327f
Thuc Tran <thuc.tv@techchain.vn>
2023-04-13 16:12:48 +0700

    feat: update 0.0.9

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it! 0.0.8" />
+      <HelloWorld msg="You did it! 0.0.9" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
